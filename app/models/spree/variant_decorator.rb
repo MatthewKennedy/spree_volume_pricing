fdcf38b21a87c1fb083b320d6503873f84283f15
Matthew Kennedy <m.kennedy@me.com>
2019-03-16 18:25:17 +0000

    use

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -10,7 +10,7 @@ Spree::Variant.class_eval do
 
     def join_volume_prices(user=nil, currency=nil)
       table = Spree::VolumePrice.arel_table
-      #currency ||= Spree::Config[:currency]
+
       if user
         Spree::VolumePrice.where(
           (table[:variant_id].eq(self.id)
@@ -28,11 +28,12 @@ Spree::Variant.class_eval do
               .and(table[:currency].eq(currency))
           ).order(position: :asc)
       end
+
     end
 
     # calculates the price based on quantity
     def volume_price(quantity, user=nil, currency=nil)
-      compute_volume_price_quantities :volume_price, self.price, quantity, user, currency
+        compute_volume_price_quantities :volume_price, self.price, quantity, user, currency
     end
 
     # return percent of earning
@@ -50,6 +51,7 @@ Spree::Variant.class_eval do
     def use_master_variant_volume_pricing?
       Spree::Config.use_master_variant_volume_pricing && !(self.product.master.join_volume_prices.count == 0)
     end
+
 
     def compute_volume_price_quantities type, default_price, quantity, user, currency
       volume_prices = self.join_volume_prices user, currency
@@ -72,37 +74,17 @@ Spree::Variant.class_eval do
     end
 
     def compute_volume_price volume_price
-      case volume_price.discount_type
-      when 'price'
         return volume_price.amount
-      when 'dollar'
-        return self.price - volume_price.amount
-      when 'percent'
-        return self.price * (1 - volume_price.amount)
-      end
     end
 
     def compute_volume_price_earning_percent volume_price
-      case volume_price.discount_type
-      when 'price'
         diff = self.price - volume_price.amount
         return (diff * 100 / self.price).round
-      when 'dollar'
-        return (volume_price.amount * 100 / self.price).round
-      when 'percent'
-        return (volume_price.amount * 100).round
-      end
     end
 
     def compute_volume_price_earning_amount volume_price
-      case volume_price.discount_type
-      when 'price'
         return self.price - volume_price.amount
-      when 'dollar'
-        return volume_price.amount
-      when 'percent'
-        return self.price - (self.price * (1 - volume_price.amount))
-      end
     end
-    
+
+
 end
