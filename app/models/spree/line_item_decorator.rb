5761c6dd6e7b2f00d99688ac9063ad458d952751
Matthew Kennedy <m.kennedy@me.com>
2019-03-17 11:46:53 +0000

    updated

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,11 +1,23 @@
 # frozen_string_literal: true
 Spree::LineItem.class_eval do
 
-  old_copy_price = instance_method(:copy_price)
+  def update_price
+    if quantity > 1
+      copy_price
+    else
+      currency_price = Spree::Price.where(
+        currency: order.currency,
+        variant_id: variant_id
+      ).first
+
+      self.price = currency_price.price_including_vat_for(tax_zone: tax_zone)
+    end
+  end
 
   define_method(:copy_price) do
 
-      if variant
+      if variant && quantity > 1
+
         if changed? && (changes.keys.include?('quantity') || changes.keys.include?('currency'))
           vprice = self.variant.volume_price(self.quantity, self.order.user, self.currency)
           if self.price.present? && vprice <= self.variant.price
@@ -16,8 +28,15 @@ Spree::LineItem.class_eval do
         if self.price.nil?
           self.price = self.variant.price
         end
-      end
 
+      else
+        currency_price = Spree::Price.where(
+          currency: order.currency,
+          variant_id: variant_id
+        ).first
+
+        self.price = currency_price.price_including_vat_for(tax_zone: tax_zone)
+      end
   end
 
 end
