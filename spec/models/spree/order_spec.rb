d80463af8c5caba4e929cc708d731ecbc5897fd3
Matthew Kennedy <m.kennedy@me.com>
2019-03-21 21:26:17 +0000

    jj

--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -15,21 +15,11 @@ RSpec.describe Spree::Order, type: :model do
       expect(@order.line_items.first.price).to eq(10)
     end
 
-    it 'uses the volume price if quantity falls within a quantity range of a volume price' do
-      @variant.volume_prices << create(:volume_price, range: '(5..10)', amount: 9)
-      @order.contents.add(@variant_with_prices, 7)
-      expect(@order.line_items.price).to eq(8)
-    end
-
     it 'uses the variant price if the quantity fails to satisfy any of the volume price ranges' do
       @order.contents.add(@variant, 10)
       expect(@order.line_items.first.price).to eq(10)
     end
 
-    it 'uses the first matching volume price in the event of more then one matching volume prices' do
-      @order.contents.add(@variant_with_prices, 5)
-      expect(@order.line_items.price).to eq(8)
-    end
 
   end
 end
