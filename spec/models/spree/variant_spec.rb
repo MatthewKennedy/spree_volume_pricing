13ef07a30a35c7de561fa184a23f4206a1ad3852
Matthew Kennedy <m.kennedy@me.com>
2019-03-21 14:30:46 +0000

    try that

--- spec/models/spree/variant_spec.rb
+++ spec/models/spree/variant_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Spree::Variant, type: :model do
     context 'discount_type = price' do
       before :each do
         @variant = create :variant, price: 10
-        @variant.volume_prices.create! amount: 9, discount_type: 'price', currency: 'USD' range: '(10+)'
+        @variant.volume_prices.create! amount: 9, discount_type: 'price', currency: 'USD', range: '(10+)'
         @role = create(:role)
         @user = create(:user)
       end
