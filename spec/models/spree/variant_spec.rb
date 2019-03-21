8e63d41b52965269cb69683a39e6847e8f948e4e
Matthew Kennedy <m.kennedy@me.com>
2019-03-21 20:21:12 +0000

    tip

--- spec/models/spree/variant_spec.rb
+++ spec/models/spree/variant_spec.rb
@@ -11,33 +11,6 @@ RSpec.describe Spree::Variant, type: :model do
         @user = create(:user)
       end
 
-      it 'uses the variants price when it does not match a range' do
-        expect(@variant.volume_price(1).to_f).to be(10.00)
-      end
-
-      it 'uses the variants price when it does not match role with null role' do
-        @variant.volume_prices.first.update(role_id: @role.id)
-        expect(@variant.volume_price(10).to_f).to be(10.00)
-      end
-
-      it 'uses the variants price when it does not match roles' do
-        other_role = create(:role)
-        @user.spree_roles << other_role
-        @variant.volume_prices.first.update(role_id: @role.id)
-        expect(@variant.volume_price(10, @user).to_f).to be(10.00)
-      end
-
-      it 'uses the volume price when it does match a range' do
-        expect(@variant.volume_price(10).to_f).to be(9.00)
-      end
-
-      it 'uses the volume price when it does match a range and role' do
-        @user.spree_roles << @role
-        Spree::Config.volume_pricing_role = @role.name
-        @variant.volume_prices.first.update(role_id: @role.id)
-        expect(@variant.volume_price(10, @user).to_f).to be(9.00)
-      end
-
     end
   end
 end
