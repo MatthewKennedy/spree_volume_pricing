687428bb27ece6caaeb6fa625ef7f6af80ccbc94
Matthew Kennedy <m.kennedy@me.com>
2019-03-21 20:43:54 +0000

    nn

--- spec/models/spree/line_item_spec.rb
+++ spec/models/spree/line_item_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe Spree::LineItem, type: :model do
   before do
     @order = create(:order)
     @variant = create(:variant, price: 10)
-    @variant.volume_prices.create! (amount: 9, discount_type: {'price'}, currency: {'USD'}, range: {'(2+)'})
+    @variant.volume_prices.create! amount: 9, discount_type: 'price', currency: 'USD', range: '(2+)'
     @order.contents.add(@variant, 1)
     @line_item = @order.line_items.first
     @role = create(:role)
