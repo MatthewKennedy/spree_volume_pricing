5ebe2d4950edad33fcd2aa461207d617fb060e37
Matthew Kennedy <m.kennedy@me.com>
2019-03-22 21:32:52 +0000

    buggered specs

--- spec/models/spree/line_item_spec.rb
+++ spec/models/spree/line_item_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Spree::LineItem, type: :model do
   before do
     @order = create(:order, currency: 'USD')
-    @variant = create(:variant, amount: 10, currency: 'USD')
+    @variant = create(:variant, price: 10, currency: 'USD')
     @variant.volume_prices.create! amount: 9, discount_type: 'price', currency: 'USD', range: '(2+)'
     @order.contents.add(@variant, 1)
     @line_item = @order.line_items.first
